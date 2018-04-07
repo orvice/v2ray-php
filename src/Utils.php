5d7e236978014795fd554865ce26401322d3b4cf
orvice <orvice@gmail.com>
2018-04-08 02:05:28 +0800

    update

--- src/Utils.php
+++ src/Utils.php
@@ -9,9 +9,10 @@ class Utils implements Contract
         return base64_encode(sprintf("%s:%s@%s:%s", $method, $uuid, $server, $port));
     }
 
-    public static function genConnString($method, $uuid, $server, $port)
+    public static function genConnString($method, $uuid, $server, $port, $obfs = "", $tls = 0, $path = "", $remark = "")
     {
         $base = self::genBaseConnInfo($method, $uuid, $server, $port);
-
+        return sprintf("vmess://%s?remarks=%s&path=%s&obfs=%s&tls=%s", $base,
+            $remark, $path, $obfs, $tls);
     }
 }
